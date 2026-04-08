a07aa8a2b3f18c657213643aa455297272528505
AzhaganNJ <azhagannatrajan2006@gmail.com>
2026-04-08 18:10:15 +0530

    refactor: streamline message handling and remove unused call functionality

--- socket/socket.js
+++ socket/socket.js
@@ -4,42 +4,33 @@ const socketHandler = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
+    // join user
     socket.on("join", (userId) => {
       users[userId] = socket.id;
       socket.userId = userId;
+
       console.log("User joined:", userId);
+      console.log("USERS:", users);
     });
 
+    // send message
     socket.on("sendMessage", ({ receiver, message }) => {
       const receiverSocket = users[receiver];
+
+      console.log("➡ Sending to:", receiver);
+      console.log("USERS:", users);
+
       if (receiverSocket) {
         io.to(receiverSocket).emit("receiveMessage", message);
+      } else {
+        console.log("Receiver not connected");
       }
     });
 
-    socket.on("callUser", ({ to, offer }) => {
-      const receiverSocket = users[to];
-      if (receiverSocket) {
-        io.to(receiverSocket).emit("incomingCall", { offer });
-      }
-    });
-
-    socket.on("answerCall", ({ to, answer }) => {
-      const receiverSocket = users[to];
-      if (receiverSocket) {
-        io.to(receiverSocket).emit("callAnswered", { answer });
-      }
-    });
-
-    socket.on("iceCandidate", ({ to, candidate }) => {
-      const receiverSocket = users[to];
-      if (receiverSocket) {
-        io.to(receiverSocket).emit("iceCandidate", { candidate });
-      }
-    });
-
+    // disconnect
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
+
       if (socket.userId) {
         delete users[socket.userId];
       }
